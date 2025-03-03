4f32c4bb6b306c95403d35b6e122c60ce3de5676
Krrish Halder <krrish.code07@gmail.com>
2025-03-03 18:48:10 +0530

    feat: Enable CORS support in the server

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cors = require('cors');
 dotenv.config();
 
 const app = express();
 
 connectDB();
 app.use(express.json());
-
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Real Estate API is running');
